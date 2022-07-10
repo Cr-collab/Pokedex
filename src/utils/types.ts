b4a55dbf12ce0a249a06019555af94a94953809c
Cr-collab <cristiccorrea@gmail.com>
2022-07-10 16:53:42 -0300

    Style: objeto que armazena as cores do background dos tipos;

---
+++ src/utils/types.ts
@@ -0,0 +1,20 @@
+export  let typesColor = {
+  bug: "#729F3F",
+  dragon :  ["#53A4CF","#F16E57"],
+  fairy: "#FDB9E9",
+  fire: "#FD7D24",
+  ghost: "#7B62A3",
+  ground:  ["#F7DE3F", "#AB9842"],
+  normal: "#A4ACAF",
+  psychic: "#F366B9",
+  steel: "#9EB7B8",
+  dark: "#707070",
+  electric: "#EED535",
+  fighting: "#D56723",
+  flying: ["#3DC7EF","#BDB9B8"],
+  grass: "#9BCC50",
+  ice: "#51C4E7",
+  poison: "#B97FC9",
+  rock: "#A38C21",
+  water: "#4592C4"
+}
